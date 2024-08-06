8d8c5b38645132faa3f4a4f57abafd62ca15969f
Robert Blackwell <reblackwell3@gmail.com>
2024-08-05 21:14:58 -0400

    update player page to change position url

--- frontend/src/pages/PlayerPage.js
+++ frontend/src/pages/PlayerPage.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+// src/pages/PlayerPage.js
+import React, { useState, useEffect } from 'react';
 import Player from '../components/Player';
 
-const PlayerPage = ( {positionUrl} ) => {
+const PlayerPage = ({ positionUrl }) => {
   const [positionNum, setPositionNum] = useState(0);
 
   const incrementPositionNum = () => {
-    console.log('incrementPositionNum');
     setPositionNum((prev) => prev + 1); // Trigger next puzzle by changing the state
   };
+
+  useEffect(() => {
+    console.log(`Position URL changed to: ${positionUrl}`);
+    // Reset positionNum or perform any necessary actions when positionUrl changes
+    setPositionNum(0);
+  }, [positionUrl]);
 
   return (
     <div>
